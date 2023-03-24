4c90aa6c47cb805798d176eb2af8543a22ad35de
Pankaj Thorat <107792523+Pankaj-Thorat@users.noreply.github.com>
2023-03-25 01:48:26 +0530

    Add files via upload

---
+++ day7/primeFactor.sh
@@ -0,0 +1,19 @@
+echo "Enter a number:"
+read n
+
+index=0
+for ((i=2; i<=$n; i++))
+do
+  while (( $n % $i == 0 ))
+  do
+    factors_arr[$index]=$i
+    index=$((index+1))
+    n=$((n/i))
+  done
+done
+
+echo "Prime factors of are:"
+for ((i=0;i<$index;i++))
+do
+  echo ${factors_arr[$i]}
+done
